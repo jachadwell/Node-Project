b48ef6c0596e891cff87714b10cacf3ead8697c0
Jared Chadwell <jared.alan.chadwell@gmail.com>
2018-12-30 00:39:15 -0500

    Added a few more html files and buttons. There are also a few more rotuing requests. Also took out the PHP file. Node sends the data to the mysql database itself rather than sending it from html to php to database.

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const express = require('express');
-const app = express();
-const router = express.Router();
-const path = require('path');
-const port = 3000;
+var express    = require('express');
+var path       = require('path');
+var bodyParser = require('body-parser');
+var mysql      = require('mysql');
+var app        = express();
+var port       = 3000;
+
+var con = mysql.createConnection({
+    host:     "localhost",
+    user:     "root",
+    password: "password",
+    database: "mydb",
+    insecureAuth: true
+});
+
+con.connect(function(err){
+    if (err) throw err;
+    console.log("Database connected");
+});
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -12,8 +29,28 @@ app.get('/profile', function(req, res){
     res.sendFile(path.join(__dirname + '/profile.html'));
 });
 
-app.get('/create_profile', function(req, res){
+app.post('/createProfile', function(req, res){
     res.sendFile(path.join(__dirname + '/create_profile.html'));
 });
 
-app.listen(port, console.log('Server is running'));
+app.post('/login', function(req, res){
+    res.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/submitProfileInfo', function(req, res){
+    var firstname = req.body.First_Name;
+    var lastname = req.body.Last_Name;
+    var username = req.body.Username;
+    var age = parseInt(req.body.Age);
+    var sql = "INSERT INTO statbuilder (First_Name, Last_Name, Username, Age) VALUES ( '" + firstname + "', '" + lastname + "', '"
+            + username + "', '" + age + "' )";
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log('1 record inserted');
+    });
+    res.redirect('/');
+});
+
+
+
+app.listen(port, console.log('Server is running...'));
