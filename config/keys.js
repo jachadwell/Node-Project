7c1c4140ef6858faeb83cb2c34457ee0c79b5db7
Jared Chadwell <jared.alan.chadwell@gmail.com>
2019-04-26 11:59:27 -0400

    Create keys.js

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    google: {
+        clientID:'1041550537143-6b7sd5abih0glfteeetp02r3bgi79ajf.apps.googleusercontent.com',
+        clientSecret: 'cqUtMwwWdUUAorvvmggVyRnD'
+    },
+    mondodb:{
+        dbURI: 'mongodb://statbuilder:password1@ds259210.mlab.com:59210/statbuilder'
+    },
+    session: {
+        cookieKey: 'lilCocks'
+    }
+} 
